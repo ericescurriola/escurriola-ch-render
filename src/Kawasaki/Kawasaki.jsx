de051191210b26362b68de288d57e3d5291127f1
Eric Escurriola <eric@escurriola.com>
2025-09-24 09:36:22 +0200

    added Kawasaki Component

---
+++ src/Kawasaki/Kawasaki.jsx
@@ -0,0 +1,58 @@
+import './Kawasaki.css';
+
+export default function Kawasaki() {
+
+  return (
+    <>
+      {/* comment */}
+      <div id="ContentPlaceHolderSection_kNaked1_Panel_Model_Slider" className="k-stream has-naked">
+        <div id="ContentPlaceHolderSection_kNaked1_Panel_Drag" className="k-label k-naked k-naked--model" data-label="ziehen" data-start="50">
+          <div className="k-naked__inner" style={{height: '440px'}}>
+            <div className="k-naked__item">
+              <img 
+                id="ContentPlaceHolderSection_kNaked1_Image_Slider_Last" 
+                src="//storage.kawasaki.eu/public/kawasaki.eu/en-EU/model/Strip 1.008.jpg"
+                alt="Kawasaki model" // Added alt attribute for accessibility
+              />
+            </div>
+            <div className="k-naked__skew" style={{width: '1291.69px', height: '440px', left: '175px'}}>
+              <div 
+                id="ContentPlaceHolderSection_kNaked1_Panel_Slider_First" 
+                className="k-naked__reskew" 
+                style={{
+                  backgroundImage: 'url("//storage.kawasaki.eu/public/kawasaki.eu/en-EU/model/Strip 2.004.jpg")',
+                  width: '1291.69px',
+                  height: '440px',
+                  left: '-176px'
+                }}
+              />
+            </div>
+          </div>
+          <div className="k-dragger ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content">
+            <span 
+              tabIndex="0" 
+              className="ui-slider-handle ui-corner-all ui-state-default" 
+              style={{left: '57%'}}
+            >
+              <div className="clip-drag">
+                <i className="kwi-al"></i>
+                <span>ziehen</span>
+                <i className="kwi-ar"></i>
+              </div>
+            </span>
+          </div>
+          <div className="k-naked__footer">
+            <a 
+              id="ContentPlaceHolderSection_kNaked1_HyperLink_ViewAllFeatures" 
+              title="FEATURES ANSEHEN" 
+              className="kw-btn kw-btn--secondary" 
+              href="//www.kawasaki.ch/de/products/Supersport___Sport/2023/Ninja_ZX-10R/features?Uid=08F0WgtdXAoNWlFZC14MXF5bXQlbDV5QCV4KDAxbDVFYCV4"
+            >
+              <span>FEATURES ANSEHEN</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
